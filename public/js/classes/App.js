581dabbcfe33d5f6ea50d417b862ab6ad28c9a3e
Alex <alexandrello@gmail.com>
2015-02-24 13:55:57 +0200

    removed unused controllers

--- public/js/classes/App.js
+++
@@ -1,8 +0,0 @@
-var App = Base.extend({
-    dependencies: [],
-    controls: {},
-    models: {},
-    init: function () {
-        this._super();
-    }
-});
--- public/js/controllers/Header.js
+++
@@ -1,5 +0,0 @@
-var Header = Controller.extend({
-    run: function () {
-        document.getElementById('content').appendChild(can.view('ejs/header', {}));
-    }
-});
