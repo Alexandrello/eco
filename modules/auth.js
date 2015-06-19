e2599f419adf574abc674b5faaabd20f7ffab126
Alex <alexandrello@gmail.com>
2015-06-19 16:25:53 +0300

    facedetect

--- modules/auth.js
+++ modules/auth.js
@@ -2,87 +2,96 @@ var User = require('../models/user');
 var crypto = require('crypto');
 
 var auth = {
-    init: function (app) {
-        app.get('/login/', function (req, res) {
-            res.render('login', {title: 'GAD'});
-        });
-
-        app.get('/register/', function (req, res) {
-            res.render('register', {title: 'GAD'});
-        });
-
-        app.post("/login/", function (req, res) {
-            var loginData = req.body;
-            if (!loginData.email || !loginData.password) {
-                res.json({
-                    success: false,
-                    error: 'Введите логин и пароль'
-                });
+    loginAction: function (req, res) {
+        auth.getAuth(req, function (auth) {
+            if (auth) {
+                res.redirect('/');
             } else {
-                loginData.password = crypto.createHash('sha256').update(loginData.password).digest('base64');
-
-                User.findOne({
-                    email: loginData.email
-                }, function (err, user) {
-
-                    console.log(user, loginData);
-
-                    if (user) {
-                        if (user.password == loginData.password) {
-                            user.sessionId = auth.generateSessionId();
-                            user.save(function(){
-                                res.json({
-                                    success: true,
-                                    sessionId: user.sessionId
-                                });
-                            });
-                        } else {
-                            res.json({
-                                success: false
-                            });
-                        }
-                    } else {
-                        res.json({
-                            success: false,
-                            error: 'Пользователь не найден'
-                        });
-                    }
-                });
+                res.render('login', {title: 'GAD'});
             }
         });
-
-        app.post("/register/", function (req, res) {
-            var loginData = req.body;
-            if (!loginData.email || !loginData.password || !loginData.name) {
-                res.json({
-                    success: false,
-                    error: 'Заполните все поля'
-                });
+    },
+    registerAction: function (req, res) {
+        auth.getAuth(req, function (auth) {
+            if (auth) {
+                res.redirect('/');
             } else {
-                loginData.password = crypto.createHash('sha256').update(loginData.password).digest('base64');
-                User.findOne({
-                    email: loginData.email
-                }, function (err, user) {
-                    if (user){
-                        res.json({
-                            success: false,
-                            error: 'Пользователь с таким email уже зарегистрирован'
-                        });
-                    } else {
-                        var user = new User(loginData);
+                res.render('register', {title: 'GAD'});
+            }
+        });
+    },
+    login: function (req, res) {
+        var loginData = req.body;
+        if (!loginData.email || !loginData.password) {
+            res.json({
+                success: false,
+                error: 'Введите логин и пароль'
+            });
+        } else {
+            loginData.password = crypto.createHash('sha256').update(loginData.password).digest('base64');
+            User.findOne({
+                email: loginData.email
+            }, function (err, user) {
+                if (user) {
+                    if (user.password == loginData.password) {
                         user.sessionId = auth.generateSessionId();
-                        user.avatar = '/img/150x150.png';
-
-                        user.save(function(){
+                        user.save(function () {
                             res.json({
                                 success: true,
                                 sessionId: user.sessionId
                             });
                         });
+                    } else {
+                        res.json({
+                            success: false
+                        });
                     }
-                });
-            }
-        });
+                } else {
+                    res.json({
+                        success: false,
+                        error: 'Пользователь не найден'
+                    });
+                }
+            });
+        }
+    },
+    register: function (req, res) {
+        var loginData = req.body;
+        if (!loginData.email || !loginData.password || !loginData.name) {
+            res.json({
+                success: false,
+                error: 'Заполните все поля'
+            });
+        } else {
+            loginData.password = crypto.createHash('sha256').update(loginData.password).digest('base64');
+            User.findOne({
+                email: loginData.email
+            }, function (err, user) {
+                if (user) {
+                    res.json({
+                        success: false,
+                        error: 'Пользователь с таким email уже зарегистрирован'
+                    });
+                } else {
+                    var user = new User(loginData);
+                    user.sessionId = auth.generateSessionId();
+                    user.avatar = '/img/150x150.png';
+
+                    user.save(function () {
+                        res.json({
+                            success: true,
+                            sessionId: user.sessionId
+                        });
+                    });
+                }
+            });
+        }
+    },
+    init: function (app) {
+        app.get('/login/', auth.loginAction);
+        app.get('/register/', auth.registerAction);
+        app.post("/login/", auth.login);
+        app.post("/register/", auth.register);
 
         app.all('*', function (req, res, next) {
             auth.getAuth(req, function (auth) {
